a30ade31f86893fd3469cf538a8d2f4ea85f724d
Richard Altmayer <richard.altmayer@asaas.com.br>
2025-10-21 16:05:53 -0300

    migration(adapt): adequações automáticas para PR #3 (Grails 2 → Grails 4)

--- grails-app/conf/spring/resources.groovy
+++ grails-app/conf/spring/resources.groovy
@@ -1,3 +1,13 @@
+import grails.core.GrailsApplication
+
 // Place your Spring DSL code here
+import org.grails.org.hibernate.query.CustomHibernateCriterionAdapter
+import com.asaas.cache.AsaasCustomCacheKeyGenerator
+
 beans = {
+
+    GrailsApplication grailsApplication
+
+    customHibernateCriterionAdapter(CustomHibernateCriterionAdapter)
+    customCacheKeyGenerator(AsaasCustomCacheKeyGenerator)
 }
