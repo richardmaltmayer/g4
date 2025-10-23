2dcaa72dfc7a8e70c39bafe3ba7dce84c6e3e2a4
Richard Altmayer <richard.altmayer@asaas.com.br>
2025-10-23 16:10:57 -0300

    Transformações via script

--- grails-app/controllers/com/asaas/interceptor/TestInterceptor.groovy
+++ grails-app/controllers/com/asaas/interceptor/TestInterceptor.groovy
@@ -1,17 +1,21 @@
 package com.asaas.interceptor
 
-class TestInterceptor implements BaseInterceptor {
 
-    TestInterceptor() {
-        match(controller: '*', action: '*')
-    }
+
 
     @Override
     Integer getInterceptorOrder() {
         return DEFAULT_PRECEDENCE_ORDER
     }
 
-    @Override
+    TestInterceptor() {
+        match(controller: '*', action: '*')
+    }
+
+    Integer getInterceptorOrder() {
+        return DEFAULT_PRECEDENCE_ORDER
+    }
+
     boolean before() {
         return true
     }
