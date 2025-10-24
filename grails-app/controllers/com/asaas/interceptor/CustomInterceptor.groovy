e58e6ddd7df16f47aa08fd544b94875a0a46a380
Richard Altmayer <richard.altmayer@asaas.com.br>
2025-10-24 08:29:57 -0300

    Refaz arquivo com base no Filters

--- grails-app/controllers/com/asaas/interceptor/CustomInterceptor.groovy
+++ grails-app/controllers/com/asaas/interceptor/CustomInterceptor.groovy
@@ -1,25 +1,17 @@
-package com.asaas.interceptor
+package web
 
-class CustomInterceptor implements BaseInterceptor {
+class CustomFilters {
 
-    CustomInterceptor() {
-        match(controller: 'custom', action: '*')
-    }
-
-    @Override
-    Integer getInterceptorOrder() {
-        return DEFAULT_PRECEDENCE_ORDER
-    }
-
-    @Override
-    boolean before() {
-        println controllerName + " / " + actionName
-        println "teste de sobrescrita 17"
-        Long customerId = 10L
-        Long customerAccountId = 900L + 100L
+    def filters = {
+        all(controller: 'custom', action: '*') {
+            before = {
+                println controllerName + " / " + actionName
+                println "teste de sobrescrita 17"
+                Long customerId = 10L
+                Long customerAccountId = 900L + 100L
                 
-        return true
+                return true
+            }
+        }
     }
-
 }
-
