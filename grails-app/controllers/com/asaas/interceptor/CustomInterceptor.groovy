9842216912b16162bea0772300c37393b09efeaf
Richard Altmayer <richard.altmayer@asaas.com.br>
2025-10-24 08:26:28 -0300

    Transformações via script

--- grails-app/controllers/com/asaas/interceptor/CustomInterceptor.groovy
+++ grails-app/controllers/com/asaas/interceptor/CustomInterceptor.groovy
@@ -1,17 +1,25 @@
-package web
+package com.asaas.interceptor
 
-class CustomFilters {
+class CustomInterceptor implements BaseInterceptor {
 
-    def filters = {
-        all(controller: 'custom', action: '*') {
-            before = {
-                println controllerName + " / " + actionName
-                println "teste de sobrescrita 17"
-                Long customerId = 10L
-                Long customerAccountId = 900L + 100L
-                
-                return true
-            }
-        }
+    CustomInterceptor() {
+        match(controller: 'custom', action: '*')
     }
+
+    @Override
+    Integer getInterceptorOrder() {
+        return DEFAULT_PRECEDENCE_ORDER
+    }
+
+    @Override
+    boolean before() {
+        println controllerName + " / " + actionName
+        println "teste de sobrescrita 17"
+        Long customerId = 10L
+        Long customerAccountId = 900L + 100L
+                
+        return true
+    }
+
 }
+
